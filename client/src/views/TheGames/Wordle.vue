a143d342276f037a64a724ce752436ea85ff1ef8
Tuan <61290445+tkene@users.noreply.github.com>
2025-12-21 14:45:25 +0100

    page pending wordle

---
+++ client/src/views/TheGames/Wordle.vue
@@ -0,0 +1,20 @@
+  
+<script>
+export default {
+    name: 'WordleGame'
+}
+</script>
+
+<template>
+  <div class="wordle-game">
+    <h1>Wordle Game - Coming Soon</h1>
+    <p>This feature is under development.</p>
+  </div>
+</template>
+
+<style scoped>
+.wordle-game {
+  padding: 20px;
+  text-align: center;
+}
+</style>
