3e734c1f389a48c129f48520e251b39676109f2c
Tuan <61290445+tkene@users.noreply.github.com>
2026-01-08 13:19:58 +0100

    fix:remake zeabur

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Point d'entrée pour Zeabur - redirige vers le serveur
 const path = require('path');
 process.chdir(path.join(__dirname, 'server'));
 require(path.join(__dirname, 'server', 'server.js'));
